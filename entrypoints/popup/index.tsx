b722169b3d155fe3e3ae8a85992c06de16d7d303
Ross Moody <fiftyfivehis@gmail.com>
2025-09-03 19:26:01 -0700

    Add Tailwind CSS to popup and update UI
    Integrated Tailwind CSS by adding dependencies, importing styles in the popup entrypoint, and configuring the Vite plugin in wxt.config.ts. Updated the popup UI to use Tailwind utility classes for styling.

--- entrypoints/popup/index.tsx
+++ entrypoints/popup/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Main } from "./main";
 
+import "./index.css";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Main />
--- entrypoints/popup/main.tsx
+++ entrypoints/popup/main.tsx
@@ -1,3 +1,8 @@
 export const Main = () => {
-  return <button>Export Data</button>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="w-40">Export Data</button>
+    </>
+  );
 };
