b7e095f6ab89c34f21e5a813720ae830118ea4a7
Ross Moody <fiftyfivehis@gmail.com>
2025-09-03 17:56:51 -0700

    Refactor popup entrypoint and update config
    Removed App.tsx and inlined popup content directly in index.tsx, renaming main.tsx to index.tsx. Updated index.html to reflect new entrypoint and metadata. Added web-ext.config.ts for extension configuration and removed it from .gitignore.

--- entrypoints/popup/index.tsx
+++ entrypoints/popup/index.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./app";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <div>Test</div>
   </React.StrictMode>
 );
---
+++ web-ext.config.ts
@@ -0,0 +1,6 @@
+import { defineWebExtConfig } from "wxt";
+
+export default defineWebExtConfig({
+  // Load the extension manually in browser of choice
+  disabled: true,
+});
